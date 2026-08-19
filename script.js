39f134bc7e68606e17ee16fa2ad696e19615747b
MTarique51 <mohdtarique7223@gmail.com>
2026-08-19 22:54:56 +0530

    Delete node_module file

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-let n = 10;
+// let n = 10;
 
-for(let i=0 ; i<n ; i++){
-    console.log("Hello," , i)
-}
+// for(let i=0 ; i<n ; i++){
+//     console.log("Hello," , i)
+// }
+// console.log("bye!")
+
+
+
+
+// let args = process.args;
+// for (let i=0; i<AbortSignal.length; i++){
+//     console.log("Hello to ", args[i]);
+// }
+
+
+
+// Exporting files //
+
+// const math = require("./math");
+
+// console.log(math);
+// console.log(math.sum(2,2));
+// console.log(math.PI);
+
+
+const info = require("./Fruits");
+console.log(info);
